876eba2c6b89568b4e97268478089c389fdc13f9
bensteUEM <Benedict.Stein@gmail.com>
2017-09-08 10:09:34 +0200

    Initial Maven Project

---
+++ src/main/java/stein/team/qlc/App.java
@@ -0,0 +1,13 @@
+package stein.team.qlc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
